82029b827bcebbd95780f738e8541332a9e1d157
Abraam00 <89326446+Abraam00@users.noreply.github.com>
2023-09-21 00:18:40 -0400

    Accordion&search bar working

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,17 +2,12 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./components/navbar";
 import SearchBar from "./components/searchbar";
 import axios from "axios";
-//import Modal from "./components/modal";
+
 // currently adding functionality that will allow for search filtering
 function Home() {
   const [injuries, setInjuries] = useState([]);
- 
-  // const [isComponentVisible, setIsComponentVisible] = useState(false);
-
-  // Function to show/hide the component
-  // const toggleComponent = () => {
-  //   setIsComponentVisible(!isComponentVisible);
-  // };
+  const [expandedIndex, setExpandedIndex] = useState(null);
+  const [search, setSearch] = useState(false);
 
   useEffect(() => {
     axios
@@ -23,50 +18,95 @@ function Home() {
 
   const [filteredElements, setFilteredElements] = useState([]);
 
-  //possibly do an alert for it? or a modal
-  const openPopup = (element) => {
-    const popupWindow = window.open("", "Popup", "width=600,height=400");
-
-    popupWindow.document.write(
-      `<html><head><title>${element.injury}</title></head><body><h3>${element.Dos}</h3></body></html>`
-    );
-    popupWindow.document.close();
-  };
-
   const handleSearch = (query) => {
     const filteredResults = injuries.filter((item) =>
       item.injury.toLowerCase().includes(query.toLowerCase())
     );
     setFilteredElements(filteredResults);
+    setSearch(true);
   };
-  /*const [iframeContent, setIframecontent] = useState(null);
-  const loadIframeContent = (element)=>
-  {
-    const customText = 'Instructions for injury...'
-    setIframecontent(textcontent);
-  };
-*/
-  console.log(injuries);
+
   return (
     <>
       <Navbar />
       <SearchBar onSearch={handleSearch} />
-      <div className="grid">
-        {filteredElements.map((injury, index) => (
-          <button
-            className="btn btn-lg btn-primary"
-            type="button"
-            key={index}
-            style={{ height: "75px" }}
-            // onClick={toggleComponent}
-            onClick={() => openPopup(injury)}
-            //onClick={() => loadIframeContent}
-          >
-            {injury.injury}
-          </button>
-        ))}
-        {/* {isComponentVisible && alert("test")} */}
-      </div>
+      {search ? (
+        <div className="grid">
+          {filteredElements.map((element, index) => (
+            <div class="accordion" id="accordionExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id={`heading${index}`}>
+                  <button
+                    class="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target={`#collapse${index}`}
+                    aria-expanded={expandedIndex === index}
+                    aria-controls={`collapse${index}`}
+                    onClick={() => {
+                      if (expandedIndex === index) {
+                        setExpandedIndex(null);
+                      } else {
+                        setExpandedIndex(index);
+                      }
+                    }}
+                  >
+                    {element.injury}
+                  </button>
+                </h2>
+                <div
+                  id={`collapse${index}`}
+                  class={`accordion-collapse collapse ${
+                    expandedIndex === index ? "show" : ""
+                  }`}
+                  aria-labelledby={`heading${index}`}
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body">{element.Dos}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="grid">
+          {injuries.map((element, index) => (
+            <div class="accordion" id="accordionExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id={`heading${index}`}>
+                  <button
+                    class="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target={`#collapse${index}`}
+                    aria-expanded={expandedIndex === index}
+                    aria-controls={`collapse${index}`}
+                    onClick={() => {
+                      if (expandedIndex === index) {
+                        setExpandedIndex(null);
+                      } else {
+                        setExpandedIndex(index);
+                      }
+                    }}
+                  >
+                    {element.injury}
+                  </button>
+                </h2>
+                <div
+                  id={`collapse${index}`}
+                  class={`accordion-collapse collapse ${
+                    expandedIndex === index ? "show" : ""
+                  }`}
+                  aria-labelledby={`heading${index}`}
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body">{element.Dos}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
