314ea5f0c567e39499f8ae0a06d1192f48295147
Rytsumusic <89271300+Rytsumusic@users.noreply.github.com>
2023-09-25 11:41:21 -0400

    formatting the navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -12,15 +12,15 @@ export default function Navbar(){
             <div className="collapse navbar-collapse" id="navbarColor01">
             <ul className="navbar-nav me-auto">
                 <li className="nav-item">
-                <a className="nav-link" href="/About">About
+                <a className="nav-link" href="/Cpr">CPR
                     <span className="visually-hidden">(current)</span>
                 </a>
                 </li>
                 <li className="nav-item">
-                <a className="nav-link" href="/ContactUs">Contact Us</a>
+                <a className="nav-link" href="/About">About</a>
                 </li>
                <li className="nav-item">
-                <a className="nav-link" href="/Cpr">CPR</a>
+                <a className="nav-link" href="/ContactUs">Contact Us</a>
                </li>
             </ul>
             </div>
