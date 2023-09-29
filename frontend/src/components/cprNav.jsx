c89391edc13e21e8ecd725768cf5584d764007c7
vakulnath <89262485+vakulnath@users.noreply.github.com>
2023-09-28 23:06:59 -0400

    all cpr info added

--- frontend/src/components/cprNav.jsx
+++ frontend/src/components/cprNav.jsx
@@ -47,64 +47,164 @@ const CprNav = () => {
                             <ul>Tap victim’s Shoulder and shout, “Are you Ok?”</ul>
                         </li>
                         <li>Activate Emergency Response system: Depending on your situation: Phone 911 or Call a Code and get an AED</li>
-                            <ul>▪ If someone comes to help you, have that person call 911 and get an AED.
-                            ▪ If you are alone and have a cell phone, call 911 and put the phone on
-                            speaker and get an AED if available.
-                            ▪ If you are alone and do not have a cell phone, leave the person to call 911
-                            and get an AED if available. Return to the person.
-                            ▪ Use the AED as soon as you have it.
+                            <ul>
+                            • If someone comes to help you, have that person call 911 and get an AED.
+                            • If you are alone and have a cell phone, call 911 and put the phone on speaker and get an AED if available.
+                            • If you are alone and do not have a cell phone, leave the person to call 911 and get an AED if available. Return to the person.
+                            • Use the AED as soon as you have it.
                             </ul>
                         <li>Check for breathing: </li>
-                            <ul>▪ Look from head to chest to see if the person is breathing
-                            ▪ Do this for at least 5 seconds but no more than 10.
-                            ▪ If the person is unresponsive and isn’t breathing normally or is only gasping
-                            then START CPR.
+                            <ul>
+                            • Look from head to chest to see if the person is breathing
+                            • Do this for at least 5 seconds but no more than 10.
+                            • If the person is unresponsive and isn’t breathing normally or is only gasping then START CPR.
                             Check Pulse:
                             • Palpate a carotid pulse
-                            • If you do not feel a pulse within 10 seconds, begin high-quality CPR,
-                            starting with chest compressions.
+                            • If you do not feel a pulse within 10 seconds, begin high-quality CPR, starting with chest compressions.
                             </ul>
                         <li>Give 30 Compressions
                         </li>
-                            <ul>▪ Make sure the person is lying on his back on a firm flat surface.
-                            ▪ Move clothes out of the way. If a person’s clothes are difficult to remove,
-                            you can still provide compressions over clothing.
-                            ▪ Put the heel of one hand on the lower half of the breastbone. Put the heel of
-                            your other hand on top of the first hand.
-                            ▪ Compress at least 2 inches at a rate of 100 to 120 compressions per
-                            minute.
-                            ▪ Do not compress deeper than 2.4 inches in adults as it may cause injuries
-                            ▪ After each compression, allow complete chest recoil.
+                            <ul>
+                            • Make sure the person is lying on his back on a firm flat surface.
+                            • Move clothes out of the way. If a person’s clothes are difficult to remove, you can still provide compressions over clothing.
+                            • Put the heel of one hand on the lower half of the breastbone. Put the heel of your other hand on top of the first hand.
+                            • Compress at least 2 inches at a rate of 100 to 120 compressions per minute.
+                            • Do not compress deeper than 2.4 inches in adults as it may cause injuries
+                            • After each compression, allow complete chest recoil.
                             </ul>
                         <li>Open the airway and give breaths</li>
-                            <ul>• Provide effective breaths: open the victim’s airway. Use the appropriate
-                            technique to open the airway
+                            <ul>
+                            • Provide effective breaths: open the victim’s airway. Use the appropriate technique to open the airway
                             • Head Tilt-Chin Lift: tilt the head back and lift the chin to open the airway
                             • Jaw Thrust: If a head or neck injury is suspected
                             Deliver each rescue breath over 1 second
                             • The victim’s chest should rise with each breath Open the airway and give breaths- continued
                             • Avoid excessive ventilation
                             • Use bag-valve mask or barrier device if available
-                            • Once an advanced airway is placed, continuous compressions are
-                            performed without pause for ventilation. Ventilations are delivered at a rate
-                            of 1 breath every 6 seconds.
+                            • Once an advanced airway is placed, continuous compressions are performed without pause for ventilation. Ventilations are delivered at a rate of 1 breath every 6 seconds.
                             • If there is a pulse and the victim is not breathing:
                             • Provide rescue breathing: 1 breath every 6 seconds (10breaths/minute)
                             </ul>
                         <li>Continue sets of compressions and breaths</li>
-                            <ul>▪ Continue giving sets of 30 compression and 2 breaths until help arrives or the person becomes responsive.
-                            ▪ In two-person CPR the rescuers should change positions after every 2 minutes
-                            ▪ Ratio stays the same for one-person & two-person CPR ▪ Use the AED as soon as you have it.</ul>
+                            <ul>
+                            • Continue giving sets of 30 compression and 2 breaths until help arrives or the person becomes responsive.
+                            • In two-person CPR the rescuers should change positions after every 2 minutes
+                            • Ratio stays the same for one-person & two-person CPR ▪ Use the AED as soon as you have it.
+                            </ul>
                     </ol>
                 </div>
                 <div className={`tab-pane fade ${activeTab === 'infant-cpr' ? 'show active' : ''}`} id="infant-cpr" role="tabpanel">
                     <h2>Infant CPR Guidelines</h2>
-                    <p>
-                    </p>
+                    <ol>
+                        <li>Make sure the scene is safe</li>
+                        <li>Check Responsiveness: Tap infant’s heel of foot and shout, “Are you Ok?”</li>
+                        <li>Activate Emergency Response system:</li>
+                            <ul>
+                            Depending on your situation: Phone 911 or Call a Code and get an AED
+                            Witnessed Arrest: If you are alone with no mobile phone, leave the victim to activate the emergency response system and get the AED before beginning CPR. Otherwise, send someone and begin CPR immediately: use the AED as soon as it is available
+                            Unwitnessed Arrest: Give 2 minutes of CPR
+                            Leave the child to activate the emergency response system and get the AED Return to the infant and resume CPR: use the AED as soon as it is available
+                            • If someone comes to help you, have that person call 911 and get an AED. ▪ If you are alone and have a cell phone, call 911 and put the phone on
+                            speaker and get an AED if available.
+                            • Use the AED as soon as you have it.
+                            </ul>
+                        <li>Check for Breathing</li>
+                            <ul>
+                            • Look from head to chest to see if the person is breathing
+                            • Do this for at least 5 seconds but no more than 10.
+                            • If the infant is unresponsive and isn’t breathing normally or is only gasping then START CPR.
+                            </ul>
+                        <li>Check Pulse:</li>
+                            <ul>
+                            • Palpate a brachial pulse
+                            • If you do not feel a pulse within 10 seconds, begin high-quality CPR, starting with chest compressions.
+                            </ul>
+                        <li>Give 30 Compressions</li>
+                            <ul>
+                            • Make sure the infant is lying on his back on a firm flat surface.
+                            • Move clothes out of the way.
+                            • Compress at least 1/3 the depth of the chest about 1 1/2 inches at rate of 100 to 120 compressions per minute
+                            • CPR ratio for one-person CPR is 30 compressions to 2 breaths
+                            • Single rescuer: use 2 fingers, 2 thumb-encircling technique or the heel of 1 hand.
+                            • CPR ratio for two-person CPR is 15 compressions to 2 breaths ▪ Use 2 Thumb-Encircling technique for compressions
+                            • After each compression, allow complete chest recoil.
+                            </ul>
+                        <li>Open the airway and give breaths</li>
+                            <ul>
+                            • Provide effective breaths: open the infant’s airway. Use the appropriate technique to open the airway
+                            • Head Tilt-Chin Lift: tilt the head back and lift the chin to open the airway
+                            • Deliver each rescue breath over 1 second
+                            • The infant’s chest should rise with each breath
+                            • Avoid excessive ventilation
+                            • Use bag-valve mask or barrier device if available
+                            • Once an advanced airway is placed, continuous compressions are performed without pause for ventilation. Ventilations are delivered at a rate of 1 breath every 2-3 seconds (20-30 breaths/minute)
+                            • If there is a pulse and the victim is not breathing:
+                            • Provide rescue breathing: 1 breath every 2-3 seconds (20-30 breaths/minute)
+                            </ul>
+                        <li>Continue sets of compressions and breaths</li>
+                            <ul>
+                            • Continue giving sets of 30 compression and 2 breaths until help arrives or the person becomes responsive.
+                            • In two-person CPR the rescuers should change positions after every 2 minutes
+                            • Ratio changes for two-person CPR to 15 compressions to 2 breaths ▪ Use the AED as soon as you have it.
+                            </ul>
+                    </ol>
                 </div>
                 <div className={`tab-pane fade ${activeTab === 'child-cpr' ? 'show active' : ''}`} id="child-cpr" role="tabpanel">
                     <h2>Child CPR Guidelines</h2>
-                    <p>Here are the CPR guidelines for children...</p>
+                    <ol>
+                        <li>Make sure the scene is safe</li>
+                        <li>Check Responsiveness: Tap child’s Shoulder and shout, “Are you Ok?”</li>
+                        <li>Activate Emergency Response system</li>
+                            <ul>
+                            Depending on your situation: Phone 911 or Call a Code and get an AED
+                            Witnessed Arrest: If you are alone with no mobile phone, leave the child to activate the emergency response system and get the AED before beginning CPR. Otherwise, send someone and begin CPR immediately: use the AED as soon as it is available
+                            Unwitnessed Arrest: Give 2 minutes of CPR
+                            Leave the child to activate the emergency response system and get the AED Return to the child and resume CPR: use the AED as soon as it is available
+                            • If someone comes to help you, have that person call 911 and get an AED. ▪ If you are alone and have a cell phone, call 911 and put the phone on speaker and get an AED if available.
+                            • Use the AED as soon as you have it.
+                        </ul>
+                        <li>Check for breathing</li>
+                            <ul>
+                            • Look from head to chest to see if the person is breathing
+                            • Do this for at least 5 seconds but no more than 10.
+                            • If the child is unresponsive and isn’t breathing normally or is only gasping
+                            then START CPR.
+                            </ul>
+                        <li>Check Pulse:</li>
+                            <ul>
+                            • Palpate a carotid pulse
+                            • If you do not feel a pulse within 10 seconds, begin high-quality CPR,
+                            starting with chest compressions.
+                            </ul>
+                        <li>Give 30 Compressions</li>
+                            <ul>
+                            • Make sure the child is lying on his back on a firm flat surface.
+                            • Move clothes out of the way. If a person’s clothes are difficult to remove, you can still provide compressions over clothing.
+                            • You may use either 1 or 2 hands for chest compressions
+                            • Compress at least 1/3 the depth of the chest about 2 inches at a rate of 100 to 120 compressions per minute.
+                            • After each compression, allow complete chest recoil.    
+                            </ul>
+                        <li>Open the airway and give breaths</li>
+                        <ul>
+                            • Provide effective breaths: open the child’s airway. Use the appropriate technique to open the airway
+                            • Head Tilt-Chin Lift: tilt the head back and lift the chin to open the airway
+                            • Jaw Thrust: If a head or neck injury is suspected
+                            • Deliver each rescue breath over 1 second
+                            • The child’s chest should rise with each breath
+                            • Avoid excessive ventilation
+                            • Use a bag-valve mask or barrier device if available
+                            • Once an advanced airway is placed, continuous compressions are
+                            performed without pause for ventilation. Ventilations are delivered at a rate of 1 breath every 2-3 seconds (20-30 breaths/minute)
+                            • If there is a pulse  higher 60 per minute and inadequate breathing: Provide rescue breathing: 1 breath every 2-3 seconds (20-30 breaths/minute)
+                        </ul>
+                        <li>Continue sets of compressions and breaths</li>
+                        <ul>
+                            • Continue giving sets of 30 compression and 2 breaths until help arrives or the person becomes responsive.
+                            • In two-person CPR the rescuers should change positions after every 2 minutes
+                            • Ratio changes for two-person CPR to 15 compressions to 2 breaths 
+                            • Use the AED as soon as you have it.
+                        </ul>
+                    </ol>
                 </div>
             </div>
         </div>
