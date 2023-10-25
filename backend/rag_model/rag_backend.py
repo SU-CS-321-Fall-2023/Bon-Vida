b7242898d462c46ab3e7b46c13b003817f7229c3
Rytsumusic <89271300+Rytsumusic@users.noreply.github.com>
2023-10-25 11:40:26 -0400

    Made a best match function just need to add in specific if its not there

--- backend/rag_model/rag_backend.py
+++ backend/rag_model/rag_backend.py
@@ -1,6 +1,7 @@
 import json
 from transformers import RagTokenizer, RagSequenceForGeneration
 import torch
+import Levenshtein
 
 # Load the RAG model
 tokenizer = RagTokenizer.from_pretrained("facebook/rag-sequence-nq")
@@ -16,27 +17,69 @@ def get_answer(query):
     answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return answer
 
+
+def get_best_match(query):
+    best_match = None
+    best_distance = float('inf')
+
+    for name in data:
+        distance = Levenshtein.distance(query, name)
+        if distance < best_distance:
+            best_match = name
+            best_distance = distance
+
+    return best_match
+
+
 def process_injury_or_illness(name):
-    if name not in data:
-        return "The specified injury or illness is not found in the dataset."
-    dos = data[name].get("Dos", [])
-    donts = data[name].get("Donts", [])
-    response = ""
+    best_match = get_best_match(name)
+    dos = data[best_match].get("Dos", [])
+    donts = data[best_match].get("Donts", [])
+    response = f"Best match: {best_match}\n\n"
 
     if dos:
+        response += "Guide Sorce: Content is provided from Advent Healths' Source of truth:\n EPIC Healthcare Electronic medical Record(EMR)\n\n"
         response += "Do's:\n" + "\n".join(dos) + "\n\n"
     if donts:
         response += "Don'ts:\n" + "\n".join(donts) + "\n\n"
+
     return response
 
 if __name__ == "__main__":
     while True:
         injury_or_illness_name = input("Please enter the name of the injury or illness (or type 'exit' to quit): ")
-        
+
         if injury_or_illness_name.lower() in ['exit', 'quit']:
             print("Exiting program. Goodbye!")
             break
-        
+
         print(process_injury_or_illness(injury_or_illness_name))
+
+
+
+
+
+
+    #if name not in data:
+        #return "The specified injury or illness is not found in the dataset."
+    #dos = data[name].get("Dos", [])
+    #donts = data[name].get("Donts", [])
+    #response = ""
+
+    #if dos:
+    #    response += "Do's:\n" + "\n".join(dos) + "\n\n"
+    #if donts:
+    #    response += "Don'ts:\n" + "\n".join(donts) + "\n\n"
+   # return response
+
+##if __name__ == "__main__":
+    #while True:
+        #injury_or_illness_name = input("Please enter the name of the injury or illness (or type 'exit' to quit): ")
+        
+        #if injury_or_illness_name.lower() in ['exit', 'quit']:
+          #  print("Exiting program. Goodbye!")
+          #  break
+        
+       # print(process_injury_or_illness(injury_or_illness_name))
 
 
